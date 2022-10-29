8be51428cf2d439a703358aed2306394ccc79469
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2022-10-29 16:12:47 +0200

    [INIT]

---
+++ src/main/java/com/moni/serviceparty/ServicePartyApplication.java
@@ -0,0 +1,13 @@
+package com.moni.serviceparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moni/serviceparty/ServicePartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moni.serviceparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
